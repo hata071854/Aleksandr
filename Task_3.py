122f814da89931bd2aa4fb6a8c9c78a95437c0f7
hata071854 <konkow85@mail.ru>
2025-01-12 10:59:07 +0300

    Add files via upload

---
+++ Task_3.py
@@ -0,0 +1,5 @@
+total_tasks = 12
+hours_spent = 1.5
+course_name = 'Python'
+average_time = hours_spent / total_tasks
+print('Курс:',course_name,',Всего задач:',total_tasks, ',затрачено времени:',hours_spent,',среднее время выполнения:',average_time,end=('.'))
